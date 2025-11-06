aab3a4651d6a4398baf15741cb482ca3e617d71d
NirjonXD <2023-2-60-113@std.ewubd.edu>
2025-11-07 03:32:33 +0600

    added some comment to the code

--- gittu.cpp
+++ gittu.cpp
@@ -1,26 +1,26 @@
 //creating class and object through c++
 #include <iostream>
 using namespace std;
-class Book{
-    public:
+class Book{  //created a class
+    public: 
     string title;
     string author;
     int year;
-    Book(string title,string author,int year){
+    Book(string title,string author,int year){ //constructor
         this->title=title ;
         this->author=author;
         this->year=year;
 
     } 
-    void displayInfo()
+    void displayInfo() //function
     {
         cout<<"TITLE : "<<title<<endl;
         cout<<"AUTHOR : "<<author<<endl;
         cout<<"Year : "<<year<<endl;
     }
 };
-int main(){
-    Book book1("Matida", "Roald Dahl", 1988);
+int main(){ //main function
+    Book book1("Matida", "Roald Dahl", 1988); //declared object and initialised the constructor
     Book book2("The Giving Tree", "Shel Silverstein", 1964);
 
     book1.displayInfo();
