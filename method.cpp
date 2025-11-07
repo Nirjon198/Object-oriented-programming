8cd537e0f3a17a8b0e64846b59679b5d4a664fab
NirjonXD <2023-2-60-113@std.ewubd.edu>
2025-11-07 13:40:42 +0600

    Added parameter

--- method.cpp
+++ method.cpp
@@ -6,19 +6,29 @@
 using namespace std;
 class Student{
     public:
+     
     void imethod(){
-        cout<<"student of EWU"<<endl;
+        cout<<"students Details"<<endl;
     }   //method defined inside the class
-
+    string name(string n1){
+        return n1;
+    }
     void omethod();
+    int id(int stdID);
 };
+// method definition outside the class
+// method_returntype Class_name::mathod_name(returntype parameter){return parameter };
 void Student::omethod(){
-    cout<<"Computer science and engineering"<<endl;
-} //method definition outside the class
-
+    cout<<"DEPARTMENT: Computer science and engineering"<<endl;
+} 
+int Student::id(int stdID){
+    return stdID;
+}
 int main(){
     Student std; //create an objcet for Student class
     std.imethod();//call the method
+    cout<<std.name("MD MEHEDI HASAN NIRJON")<<endl;
     std.omethod();
+    cout<<"STUDENT ID : "<<std.id(113)<<endl; //display method parameter details cout<<object_name.method_name(parameter value);
     return 0;
 }
