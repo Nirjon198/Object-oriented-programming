fb4a5fd9f75ca361a3d732bc057c525aa454a31f
NirjonXD <2023-2-60-113@std.ewubd.edu>
2025-11-16 16:38:46 +0600

    iterator

---
+++ iterator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>num={1,2,3,4,5};
+    cout << *num.begin(); // Iterators are used to access and iterate through elements of data structures (vectors, sets, etc.), by "pointing" to them
+    return 0;
+}
