68c2544c72e067c8cf5eaa8aa3a36997b1df55d2
NirjonXD <2023-2-60-113@std.ewubd.edu>
2025-11-16 16:53:14 +0600

    update-0

--- iterator.cpp
+++ iterator.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// int main(){
+//     vector<int>num={1,2,3,4,5};
+//     cout << *num.begin(); // Iterators are used to access and iterate through elements of data structures (vectors, sets, etc.), by "pointing" to them
+//     return 0;
+// }
 int main(){
-    vector<int>num={1,2,3,4,5};
-    cout << *num.begin(); // Iterators are used to access and iterate through elements of data structures (vectors, sets, etc.), by "pointing" to them
+    //create a vector 
+    vector<string>cars={"BMW","VOLVO","FORD","MAZDA"};
+    //crete a vector iterator call it
+    vector<string>::iterator it;
+    //loop through the vector will iterator
+    for(it=cars.begin();it!=cars.end();it++){
+        cout<<*it<<endl;
+    }
     return 0;
+
 }
